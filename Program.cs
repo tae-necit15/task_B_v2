b45a9d65a0bcae24dd3fdfbf058c43f2f5992e4f
tae-necit15 <70203126+tae-necit15@users.noreply.github.com>
2020-10-17 22:12:52 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n_serv = 6;
+            int sum = 0;
+            float sum_n = 0;
+            int[] arr_serv = new int[]{5, 1, 6, 2, 4};
+            for (int i = 0; i < (n_serv - 1); i++) {
+                sum += arr_serv[i];
+            }
+            sum_n = n_serv * (n_serv + 1) / 2;
+            Console.WriteLine(sum_n - sum);
+
+            Console.ReadKey();
+            
+        }
+    }
+}
